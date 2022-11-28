5ea5ddf3a31025edbfc0fce431d4772eeec7f188
Jamie Omondi <cruiseomondi90@gmail.com>
2022-11-28 21:16:57 +0300

    I have started with the test also 😎

---
+++ app/src/androidTest/java/com/code/jamie/noteme/ui/fragments/AuthFragmentTest.kt
@@ -0,0 +1,36 @@
+package com.code.jamie.noteme.ui.fragments
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class AuthFragmentTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun onCreate() {
+//        onView(withId(R.id.))
+    }
+
+    @Test
+    fun onViewCreated() {
+    }
+
+    @Test
+    fun onCreateView() {
+    }
+}
