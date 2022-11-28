f372093ca150c1d06222cc9758d6703af3800936
Jamie Omondi <cruiseomondi90@gmail.com>
2022-11-28 22:07:29 +0300

    Again changed

--- app/src/main/java/com/code/jamie/noteme/room/NoteMeDB.kt
+++ app/src/main/java/com/code/jamie/noteme/room/NoteMeDB.kt
@@ -3,9 +3,10 @@ package com.code.jamie.noteme.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.code.jamie.noteme.models.NoteDB
 import com.code.jamie.noteme.vo.models.User
 
-@Database(entities = [User::class], version = 1, exportSchema = false)
+@Database(entities = [NoteDB::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class NoteMeDB : RoomDatabase() {
     abstract fun noteMeDao(): NoteMeDao
